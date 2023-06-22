92c7ec5fa4d10d759b12272ae9c3a9ddf8abcf3b
Xavier Aguilar <kevinjkevps4@gmail.com>
2023-06-21 19:45:18 -0500

    taller20 Caracola inicio

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <!-- Aqui utilizamos dentro de la pagina el componente es como un elemento html -->
-  <!-- cada componente maneja una sesion independiente entre ellos, por lo que al modificar con f12 el otro no cambia -->
-  <!-- <Contador titulo="Estudiante" valor2="2"/> estos se transforman en atributos del contador -->
-  <Contador titulo="Estudiante" v-bind:inicio="400+2"/> <!--si queremos que todo lo que mandemos se comporte como un javascript debemos mandar con v-bind-->
-  <Contador titulo="Profesor" :inicio="101"/> <!--el atajo del v-bind es :-->
-
-  <!-- <CalculadoraDeber /> -->
+  <CaracolaMagica/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 //import CalculadoraDeber from './components/CalculadoraDeber.vue' //se referencia y se da el nombre del componentepuede ser otro pero es mejor el mismo
-import Contador from './components/Contador.vue'
+import CaracolaMagica from './components/CaracolaMagica.vue'
 export default {
   // lo que esta aqui son los OptionApi
   name: 'App',
   components: {
     // HelloWorld
-    Contador //el mismo nombre de arriba al importar
+    // Contador el mismo nombre de arriba al importar
     // CalculadoraDeber
+    CaracolaMagica
   }
 }
 </script>
---
+++ src/components/CaracolaMagica.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>La Caracola Magicaaaaaaaaaa</h1>
+    <img src="https://via.placeholder.com/250" alt="No se puede presentar la imagen">
+    <input v-model="pregunta" type="text" placeholder="Hazme una pregunta">
+    <p>Recuerda dar enter al terminar</p>
+    <div>
+        <h2>Voy a pasar de año?</h2>
+        <h1>SI,NO</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pregunta: 'Hola mundo'
+        }
+    }
+}
+</script>
+
+<style></style>
